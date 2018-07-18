532d8372fe18338515f53419222c0c835a7ccb74
skorytnicki <szymonkorytnicki@gmail.com>
2018-07-19 00:55:54 +0200

    Added support for lists

--- createElement.js
+++ createElement.js
@@ -1,14 +1,27 @@
 const basicComponentsList = require('./componentsList');
 const Block = require('./components/Block');
 
+// todo inspect this; harmonize this API
 async function createElement(component, props, ...children) {
-    if(basicComponentsList.includes(component.name)) {
+    if (basicComponentsList.includes(component.name)) {
         return component(props, ...children)
     }
 
     const Component = new component(props);
     const result = await Component.render();
-    return await createElement(Block, {}, [result]); // todo inspect this; harmonize this API
+
+    if (!Array.isArray(result)) { // component returned single element
+        return result;
+    }
+
+    // It is Block or array of elements (.map(async (el) => <Text>Hey {el}</Text>))
+    // todo: testing!
+    const components = [];
+    for (let func of result) {
+        components.push(await func);
+    }
+    return components;
+
 }
 
 module.exports = createElement;
